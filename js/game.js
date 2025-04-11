97a02989a4710d91327c7aea4e3e5caee9919c22
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-04-11 12:48:28 -0400

    finished -01

---
+++ js/game.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Aimar Fernandez
+// Created on: Apr 2025
+// This file contains the JS functions for index.html
+
+/* global Phaser */
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    backroundColor: 0x5f6e7a
+}
+const game = new Phaser.Game(config)
+console.log(game)
