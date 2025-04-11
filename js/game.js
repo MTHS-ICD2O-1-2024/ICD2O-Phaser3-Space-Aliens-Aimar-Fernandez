7b60b1ae851da737373ac28892e5d867065fed3c
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-04-11 17:24:26 +0000

    fixed js format

--- js/game.js
+++ js/game.js
@@ -7,20 +7,21 @@
 // This file contains the JS functions for index.html
 
 const config = {
-    type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
-    physics: {
-        default: "arcade",
-        arcade: {
-            debug: true,
-        },
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
     },
-    backroundColor: 0x5f6e7a,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-    },
+  },
+  backroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
+
 const game = new Phaser.Game(config)
 console.log(game)
