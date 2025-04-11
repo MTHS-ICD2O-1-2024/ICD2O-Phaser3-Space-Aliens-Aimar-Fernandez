84288c98b9dad54bd180f8aa1a9043ec19b7a4e6
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-04-11 17:14:59 +0000

    added js code

--- js/game.js
+++ js/game.js
@@ -10,7 +10,17 @@ const config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
-    backroundColor: 0x5f6e7a
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true,
+        },
+    },
+    backroundColor: 0x5f6e7a,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
 }
 const game = new Phaser.Game(config)
 console.log(game)
