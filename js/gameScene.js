9a308ec8c2da25ddfb9682ae7a5fab3bbfe20f56
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-05-15 16:40:33 +0000

    added alien

--- js/gameScene.js
+++ js/gameScene.js
@@ -7,6 +7,16 @@
 // This is the game scene
 
 class GameScene extends Phaser.Scene {
+  createAlien () {
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1
+    const anAlien = this.physics.add.sprite(alienXLocation, 100, 'alien')
+    anAlien.body.velocity.y = 200
+    anAlien.body.velocity.x = alienXVelocity
+    this.alienGroup.add(anAlien)
+  }
+
   constructor () {
     super({ key: 'gameScene' })
     this.background = null
@@ -20,10 +30,14 @@ class GameScene extends Phaser.Scene {
 
   preload () {
     console.log('Game Scene')
+    //images
     this.load.image('starBackground', './assets/starBackground.png')
     this.load.image('ship', './assets/spaceShip.png')
     this.load.image('missile', './assets/missile.png')
+    this.load.image('alien', './assets/alien.png')
+    //sound
     this.load.audio('laser', './assets/laser1.wav')
+    
   }
 
   create (data) {
@@ -31,6 +45,8 @@ class GameScene extends Phaser.Scene {
     this.background.setOrigin(0, 0)
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
     this.missileGroup = this.physics.add.group()
+    this.alienGroup = this.add.group()
+    this.createAlien()
   }
 
   update (time, delta) {
@@ -62,7 +78,7 @@ class GameScene extends Phaser.Scene {
     }
     this.missileGroup.children.each(function (item) {
       item.y = item.y - 15
-      if (item.y < 0 ) {
+      if (item.y < 0) {
         item.destroy()
       }
     })
